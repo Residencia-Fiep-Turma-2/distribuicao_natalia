f496fbc84cd29a3ae35eae71b97269561e3e4031
nataliaasaa <nataliaasaa@gmail.com>
2020-08-26 11:22:36 -0300

    Add files via upload

---
+++ exibir_numeros.py
@@ -0,0 +1,12 @@
+def naturaisImpares():
+    for i in range(1, 100):
+        if (i % 2 != 0):
+            print ("Numeros naturais impares de 0 a 100:", i)
+
+naturaisImpares()
+
+def naturais():
+    for i in range(1, 51):
+        print ("Numeros naturais de 0 a 50: ", i)
+
+naturais()
